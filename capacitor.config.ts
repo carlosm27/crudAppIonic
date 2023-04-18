c13a3ba4b27dde0ecfc6588531f6e78f0e5bb247
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-18 12:25:51 -0400

    Create Ionic App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { Server } from 'http';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'todoAppIonic',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    "androidScheme": "http",
+    "allowNavigation": [
+      'https://d245-190-120-248-136.ngrok-free.app/todo_tasks'
+    ]
+  }
+  
 };
+
 
 export default config;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import TaskApp from '../components/TaskList';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Todo List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">ToDo List</IonTitle>
           </IonToolbar>
+          <TaskApp></TaskApp>
         </IonHeader>
         <ExploreContainer />
       </IonContent>
